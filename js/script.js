9729363200bc940880206855738ec6df5aaf0d3d
Gabriel Rikson <gabrielriksonti@gmail.com>
2021-12-16 14:25:01 -0300

    Ajustes CSS e menu mobile

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 $(function(){
     const btnMenu       = $('.menu-mobile');
     const interfaceMenu = $('.menu-mobile ul');
-
+    
     btnMenu.click(function(){
-        interfaceMenu.slideToggle();
+        interfaceMenu.slideToggle(0300);
     });
+})
+
+$(function(){
+    const header = $('header');
+    const menuMobileUl = $('nav.menu-mobile ul');
+    const menuMobileLi = $('nav.menu-mobile li');
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 150){
+            header.css('padding','10px 0');
+            header.css('background-color','#ff966c');
+            menuMobileUl.css('top',60);
+            menuMobileLi.css('background-color','#ff966c');
+            menuMobileLi.css('border-top','1px solid #e48864');
+        }
+        else{
+            header.css('background-color','rgba(46, 46, 46, 0.698)');
+            header.css('padding','18px 0');
+            menuMobileUl.css('top',75);
+            menuMobileLi.css('background-color','rgb(37, 34, 34)');
+            menuMobileLi.css('border-top','1px solid rgb(51, 51, 51)');
+        }
+    })
 })
 
 //aqui inicia o script de animação de slide do conteudo
